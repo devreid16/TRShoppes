17e5cca205e906670a86fe606aaca24a3a444d1a
Angela Reid <reidang94@gmail.com>
2016-08-10 21:56:44 -0400

    add view and grouping for customer

--- TRShoppes/Controllers/HomeController.cs
+++ TRShoppes/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TRShoppes.DAL;
+using TRShoppes.ViewModels;
 
 namespace TRShoppes.Controllers
 {
     public class HomeController : Controller
     {
+        private ShopContext db = new ShopContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +19,20 @@ namespace TRShoppes.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<PurchaseDateGroup> data = from customer in db.Customers
+                                                   group customer by customer.PurchaseDate into dateGroup
+                                                   select new PurchaseDateGroup()
+                                                   {
+                                                       PurchaseDate = dateGroup.Key,
+                                                       CustomerCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
+        }
 
-            return View();
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         public ActionResult Contact()
---
+++ TRShoppes/ViewModels/PurchaseDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TRShoppes.ViewModels
+{
+    public class PurchaseDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? PurchaseDate { get; set; }
+
+        public int CustomerCount { get; set; }
+    }
+
+}
